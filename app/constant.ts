307c4008a69f9fdba659d4986f2fea6d881d1cd8
Alexey.Komarov <alexey.komarov@ringcentral.com>
2024-05-13 22:38:57 +0400

    fix cut off dates

--- app/constant.ts
+++ app/constant.ts
@@ -135,9 +135,10 @@ export const GEMINI_SUMMARIZE_MODEL = "gemini-pro";
 
 export const KnowledgeCutOffDate: Record<string, string> = {
   default: "2021-09",
-  "gpt-4-turbo": "2023-12",
-  "gpt-4-turbo-2024-04-09": "2023-12",
-  "gpt-4-turbo-preview": "2023-12",
+  "gpt-4o": "2023-10",
+  "gpt-4-turbo": "2023-04",
+  "gpt-4-turbo-2024-04-09": "2023-04",
+  "gpt-4-turbo-preview": "2023-04",
   "gpt-4-vision-preview": "2023-04",
   // After improvements,
   // it's now easier to add "KnowledgeCutOffDate" instead of stupid hardcoding it, as was done previously.
