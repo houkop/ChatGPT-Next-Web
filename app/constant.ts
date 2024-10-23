d3a1a3f765e66b053cd1ad22c3b4ec1f8e462d0a
Alexey.Komarov <alexey.komarov@ringcentral.com>
2024-10-23 14:57:30 +0400

    anthropic return

--- app/constant.ts
+++ app/constant.ts
@@ -288,13 +288,7 @@ const googleModels = [
 ];
 
 const anthropicModels = [
-  "claude-instant-1.2",
-  "claude-2.0",
-  "claude-2.1",
-  "claude-3-sonnet-20240229",
-  "claude-3-opus-20240229",
-  "claude-3-haiku-20240307",
-  "claude-3-5-sonnet-20240620",
+  "claude-3-5-sonnet-20241022",
 ];
 
 const baiduModels = [
@@ -385,17 +379,17 @@ export const DEFAULT_MODELS = [
   //    sorted: 3,
   //   },
   // })),
-  // ...anthropicModels.map((name) => ({
-  //   name,
-  //   available: true,
-  //  sorted: seq++,
-  //   provider: {
-  //     id: "anthropic",
-  //     providerName: "Anthropic",
-  //     providerType: "anthropic",
-  //    sorted: 4,
-  //   },
-  // })),
+  ...anthropicModels.map((name) => ({
+    name,
+    available: true,
+   sorted: seq++,
+    provider: {
+      id: "anthropic",
+      providerName: "Anthropic",
+      providerType: "anthropic",
+     sorted: 4,
+    },
+  })),
   // ...baiduModels.map((name) => ({
   //   name,
   //   available: true,
